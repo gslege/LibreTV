76a5694eb6ebbb3762332bf28b02f224313ce7d6
GSlege <91965195+gslege@users.noreply.github.com>
2025-10-18 17:09:53 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,14 +1,33 @@
 const CUSTOMER_SITES = {
    qiqi: { 
-        api: 'https://collect.wolongzyw.com/api.php/provide/vod/',
+        api: 'https://collect.wolongzyw.com/api.php/provide/vod',
         name: '默认资源',
          }，
 
    qiqi: { 
-        api: 'https://api.apibdzy.com/api.php/provide/vod/',
+        api: 'https://api.apibdzy.com/api.php/provide/vod',
         name: '百度资源',
-         }
+         },
 
+   qiqi: { 
+        api: 'https://api.guangsuapi.com/api.php/provide/vod/from/gsm3u8',
+        name: '光速资源',
+         },
+
+   qiqi: { 
+        api: 'https://jyzyapi.com/provide/vod/from/jinyingm3u8',
+        name: '金鹰资源',
+         },
+
+   qiqi: { 
+        api: 'https://360zy.com/api.php/provide/vod',
+        name: '360资源',
+         },
+
+   qiqi: { 
+        api: 'https://wwzy.tv/api.php/provide/vod',
+        name: '旺旺短剧',
+         }
 };
 
 // 调用全局方法合并
